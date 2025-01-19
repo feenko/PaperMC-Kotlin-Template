e46f4f218dcaa651a1e3a5db54784df94a064806
Szymon Pel <szymonpel@proton.me>
2025-01-19 11:55:56 +0100

    style: apply isort and black

--- bootstrap/__init__.py
+++ bootstrap/__init__.py
@@ -1,13 +1,7 @@
 from .project import ProjectSetup
 from .styles import Colors, Styles
 from .ui import get_input, print_header
-from .validators import (
-    to_kebab,
-    validate_group_id,
-    validate_kotlin_version,
-    validate_project_name,
-    validate_version,
-)
+from .validators import to_kebab, validate_group_id, validate_kotlin_version, validate_project_name, validate_version
 
 __all__ = [
     "ProjectSetup",
