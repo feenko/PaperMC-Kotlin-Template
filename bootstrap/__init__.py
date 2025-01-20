f6e1f2ffe6a8c0a228beec13069dd8e2b116a431
Szymon Pel <szymonpel@proton.me>
2025-01-20 14:20:46 +0100

    style(bootstrap): format code

--- bootstrap/__init__.py
+++ bootstrap/__init__.py
@@ -1,7 +1,7 @@
 from .project import ProjectSetup
 from .styles import Colors, Styles
 from .ui import get_input, print_header
-from .validators import to_kebab, validate_group_id, validate_kotlin_version, validate_yes_no, validate_project_name, validate_version
+from .validators import to_kebab, validate_group_id, validate_kotlin_version, validate_project_name, validate_version, validate_yes_no
 
 __all__ = [
     "ProjectSetup",
