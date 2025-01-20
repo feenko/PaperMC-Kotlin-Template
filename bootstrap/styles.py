ad9bb7ccc8b7e5d8021ff6137d8a16fa5b7a2414
Szymon Pel <szymonpel@proton.me>
2025-01-20 14:28:35 +0100

    fix(bootstrap): remove unused colors and styles

--- bootstrap/styles.py
+++ bootstrap/styles.py
@@ -1,16 +1,12 @@
 class Colors:
-    BLUE = "\033[94m"
     CYAN = "\033[96m"
     GREEN = "\033[92m"
-    YELLOW = "\033[93m"
     RED = "\033[91m"
     BOLD = "\033[1m"
     DIM = "\033[2m"
     GRAY = "\033[90m"
-    DARK_GRAY = "\033[90m"
     RESET = "\033[0m"
 
 
 class Styles:
     LIGHT_LINE = "─"
-    ARROW = ">"
